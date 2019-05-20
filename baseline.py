f6249dd091ed1b707f710bdf3d0e746f72bf1e68
John Zhang <798105509@qq.com>
2019-05-20 16:06:57 +0800

    fix bugs

--- baseline.py
+++ baseline.py
@@ -4,48 +4,57 @@ import tensorflow as tf
 
 # Model configs
 DIMEN = 128 
+kernel_size_1 = (3,3)
+pool_size_1 = (2,2)
+strides = 1
 
-input_shape = ((DIMEN**2) * 3,)
-convolution_shape = (DIMEN,DIMEN,3)
-kernel_size_1 = (4,4)
-kernel_size_2 = (3,3)
-pool_size_1 = (3,3)
-pool_size_2 = (2,2)
-strides = 2
+
+# input_shape = ((DIMEN**2) * 3,)
+# convolution_shape = (DIMEN,DIMEN,3)
+
+input_shape = (128,128,3)
 
 p_input = layers.Input(shape=input_shape)
 g_input = layers.Input(shape=input_shape)
 
+# reshape = layers.Reshape(input_shape=input_shape , target_shape=convolution_shape)
+# p_input, g_input = reshape(p_input), reshape(g_input)
 
 # Conv layers begin
 # Conv 1-3
 
-shared_blue = [
-	# Reshape(input_shape=input_shape , target_shape=convolution_shape),
-	layers.Conv2D(32, kernel_size=kernel_size_1, strides=strides, activation='relu'),
-    layers.MaxPooling2D(pool_size=pool_size_1, strides=strides),
+block1 = [
+	layers.Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'),
+    layers.Conv2D(64, kernel_size=(3,3), padding='same', activation='relu'),
+    layers.MaxPooling2D((2,2), strides=(2,2)),
 ]
-BlueUnit1 = tf.keras.Sequential(shared_blue)
-BlueUnit2 = tf.keras.Sequential(shared_blue)
-BlueUnit3 = tf.keras.Sequential(shared_blue)
+block2 = [
+	layers.Conv2D(128, kernel_size=(3,3), padding='same', activation='relu'),
+    layers.Conv2D(128, kernel_size=(3,3), padding='same', activation='relu'),
+    layers.MaxPooling2D((2,2), strides=(2,2)),
+]
+block3 = [
+	layers.Conv2D(256, kernel_size=(3,3), padding='same', activation='relu'),
+    layers.Conv2D(256, kernel_size=(3,3), padding='same', activation='relu'),
+    layers.MaxPooling2D((2,2), strides=(2,2)),
+]
+BlueUnit1 = tf.keras.Sequential(block1)
+BlueUnit2 = tf.keras.Sequential(block2)
+BlueUnit3 = tf.keras.Sequential(block3)
 
-p = BlueUnit1(p_input)
-g = BlueUnit1(g_input)
-p = BlueUnit2(p)
-g = BlueUnit2(g)
-p = BlueUnit3(p)
-g = BlueUnit3(g)
+p,g = BlueUnit1(p_input),BlueUnit1(g_input)
+# import pdb; pdb.set_trace()
+p,g = BlueUnit2(p),BlueUnit2(g)
+p,g = BlueUnit3(p),BlueUnit3(g)
 
 # Conv4
 
-conv1 = layers.Conv2D(32, kernal_size=kernal_size_1, strides=strides, activation='relu')
-p_bn1 = layers.batch_normalization()
-g_bn1 = layers.batch_normalization()
+conv1 = layers.Conv2D(32, kernel_size=(3,3), activation='relu')
+p_bn1 = layers.BatchNormalization()
+g_bn1 = layers.BatchNormalization()
 
-p = conv1(p)
-g = conv1(g)
-p = p_bn1(p)
-g = g_bn1(g)
+p,g = conv1(p),conv1(g)
+p,g = p_bn1(p),g_bn1(g)
 
 shared_green = [
     layers.ReLU(),
@@ -54,8 +63,7 @@ shared_green = [
 
 GreenUnit1 = tf.keras.Sequential(shared_green)
 
-p = GreenUnit1(p)
-g = GreenUnit1(g)
+p,g = GreenUnit1(p),GreenUnit1(g)
 
 # SEBlock Begins here
 p_channel = K.int_shape(p)[-1]
@@ -71,20 +79,18 @@ shared_se = [
 
 SE1 = tf.keras.Sequential(shared_se)
 p_se = SE1(p)
-g_se = SE2(g)
+g_se = SE1(g)
 p = layers.Multiply()([p, p_se])
 g = layers.Multiply()([g, g_se])
 
 # Conv5
 
-conv2 = layers.Conv2D(32, kernal_size=kernal_size_1, strides=strides, activation='relu')
-p_bn2 = layers.batch_normalization()
-g_bn2 = layers.batch_normalization()
+conv2 = layers.Conv2D(32, kernel_size=kernel_size_1, strides=strides, activation='relu')
+p_bn2 = layers.BatchNormalization()
+g_bn2 = layers.BatchNormalization()
 
-p = conv2(p)
-g = conv2(g)
-p = p_bn2(p)
-g = g_bn2(g)
+p,g = conv2(p),conv2(g)
+p,g = p_bn2(p),g_bn2(g)
 
 GreenUnit2 = tf.keras.Sequential(shared_green)
 p = GreenUnit2(p)
@@ -99,7 +105,7 @@ g = layers.Multiply()([g, g_se])
 # Conv layers finished
 
 # Element-wise subtraction
-sub = layers.Subtraction()([p,g])
+sub = layers.Subtract()([p,g])
 
 # Dense layers
 x = layers.Dense(64, activation='sigmoid')(sub)
