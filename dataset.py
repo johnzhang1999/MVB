b7e92d567cde840e77a726c7d0eacec1632638bb
John Zhang <xzjjohn2011@gmail.com>
2019-05-24 03:43:58 +0000

    trains well!

--- dataset.py
+++ dataset.py
@@ -31,79 +31,21 @@ class Dataset(object):
   def MVBDataset(self, generator: Generator):
     lib = generator.lib
     img_count = len(lib.items())
-    # # generating positive pairs
-    # pos = []
-
-    # for img,val_dict in lib.items():
-    #     ps = val_dict['probe']
-    #     gs = val_dict['gallery']
-    #     for p in ps:
-    #         for g in gs:
-    #             pos.append((p,g)) # CHECK: tuple or list?
-    # print('pos pair num:', len(pos))
-
-    # # generating negative pairs
-
-    # # ENSURE a balanced pos and neg pair ratio for training
-    # neg_sample_num = len(pos) // len(lib.keys())
-    # neg = []
-
-    # def get_random_not(lib,img):
-    #     k,v = random.choice(list(lib.items()))
-    #     if k == img: get_random_not(lib,img)
-    #     return v
-
-    # for img,val_dict in lib.items():
-    #     ps =  val_dict['probe']
-    #     for i in range(neg_sample_num):
-    #         rand_neg_gs = get_random_not(lib,img)['gallery']
-    #         neg.append((random.choice(ps),random.choice(gs)))
-
-    # print('neg pair num:', len(neg))
 
     # building a tf dataset
     dataset = tf.data.Dataset.from_generator(generator.get_next, 
                 output_types=((tf.string, tf.string),tf.int8))
 
-
-    # # building tf dataset
-    # pos_labels = np.ones(len(pos))
-    # neg_labels = np.zeros(len(neg))
-    # labels = np.concatenate([pos_labels,neg_labels])
-    # label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(labels, tf.int64))
-    # # label_ds = tf.data.Dataset.from_tensor_slices(labels)
-    # for label in label_ds.take(10):
-    #   print(label.numpy())
-
-    # imgs = pos + neg
-    # # print(imgs[0][0])
-    # img_count = len(imgs)
-    # img_ds = tf.data.Dataset.from_tensor_slices(imgs)
     dataset = dataset.map(self._load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
     print('shape: ', repr(dataset.output_shapes))
     print('type: ', dataset.output_types)
     print()
     print(dataset)
-    
-    # # zipping the labels and images
-    # image_label_ds = tf.data.Dataset.zip((img_ds, label_ds))
-    # print('image shape: ', image_label_ds.output_shapes[0])
-    # print('label shape: ', image_label_ds.output_shapes[1])
-    # print('types: ', image_label_ds.output_types)
-    # print()
-    # print(image_label_ds)
 
     # visual examinations
     if self.preview: self.preview_dataset(dataset)
-      
-    
-    # ds = image_label_ds.cache()
-    # ds = dataset
-    # ds = ds.apply(
-    #   tf.data.experimental.shuffle_and_repeat(buffer_size=round(img_count//1.7)))
-    # if shuffle:
-    #   # can reduce buffer_size if memory runs out
-    #   ds = image_label_ds.shuffle(buffer_size=round(img_count/1.5))
+
+    # repeat, batch and prefetch
     ds = dataset.repeat()
     ds = ds.batch(self.batch_size)
     if self.prefetch:
@@ -126,4 +68,4 @@ class Dataset(object):
       plt.yticks([])
       plt.xlabel(str(m.numpy()))
     plt.show()
-    plt.savefig(path)
+    if save: plt.savefig(path)
--- train.py
+++ train.py
@@ -7,60 +7,41 @@ from generator import Generator
 import tensorflow as tf
 import time
 
-BATCH_SIZE = 128
-VAL_SIZE = 100
+BATCH_SIZE = 32
+VAL_SIZE = 500
 
 # dataset
 
-# dataset_train,num_train = MVBDataset(mode='train', preview=True, shuffle=True, batch_size=BATCH_SIZE)
 train_gen = Generator(mode='train')
 # print 2 outputs from our generator just to see that it works:
 # iter = generator.get_next()
 # for i in range(2):
 #     print(next(iter))
 train = Dataset(train_gen,batch_size=BATCH_SIZE)
-dataset_train = train.data
-num_train = train.img_count
-# model_input = ds.next_element
-# print(model_input)
-# model = Model(model_input)
+dataset_val = train.data.take(VAL_SIZE)
+dataset_train = train.data.skip(VAL_SIZE)
+num_train = train.img_count - VAL_SIZE
 
 test_gen = Generator(mode='test')
 test = Dataset(test_gen,batch_size=BATCH_SIZE)
 dataset_test = test.data
 num_test = test.img_count
 
-
-
-# dataset_val = dataset_train.take(VAL_SIZE)
-# dataset_train = dataset_train.skip(VAL_SIZE)
-# num_train -= VAL_SIZE
-# dataset_test,num_test = MVBDataset(mode='test', preview=True, shuffle=True, batch_size=BATCH_SIZE)
-# print('SIZES:',num_train,VAL_SIZE,num_test)
-# min dataset
-# num_train = 3000
-# dataset_train = dataset_train.take(num_train)
-# dataset_val = dataset_train.skip(num_train).take(VAL_SIZE)
-# num_test = 300
-# dataset_test = dataset_train.skip(num_train).skip(VAL_SIZE).take(num_test)
-
-steps_per_epoch = num_train//BATCH_SIZE
-
 # model
 
 model = baseline()
 # model = multi_gpu_model(model, gpus=2)
 model.compile(loss=losses.binary_crossentropy, 
-                optimizer=optimizers.Adam(lr=0.001), 
+                optimizer=optimizers.Adam(lr=0.0001), 
                 metrics=['accuracy'])
 print(model.summary())
 # plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)  
 
-# filepath = '../checkpoints/saved-model-{epoch:02d}-{val_acc:.2f}.hdf5'
+filepath = '../checkpoints/saved-model-{epoch:02d}-{val_acc:.2f}.hdf5'
 
 callbacks = [
   # Interrupt training if `val_loss` stops improving for over 2 epochs
-  # tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+  tf.keras.callbacks.EarlyStopping(patience=10, monitor='val_loss'),
   # Write TensorBoard logs to `./logs` directory
   tf.keras.callbacks.TensorBoard(log_dir='/output', histogram_freq=0, 
                                   embeddings_freq=0, update_freq='batch'),
@@ -69,8 +50,8 @@ callbacks = [
   #                                       mode='auto', period=1)
 ]
 
-model.fit(dataset_train, epochs=1, steps_per_epoch=num_train//BATCH_SIZE, callbacks=callbacks)
-            # validation_data=dataset_val, validation_steps=VAL_SIZE//BATCH_SIZE)
+model.fit(dataset_train, epochs=100, steps_per_epoch=num_train//BATCH_SIZE, callbacks=callbacks,
+            validation_data=dataset_val, validation_steps=VAL_SIZE//BATCH_SIZE)
  
 # EVALUATION
 print('### EVALUATION ###')
